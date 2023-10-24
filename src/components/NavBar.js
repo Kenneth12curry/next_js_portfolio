75ea8e19f46aba31df3c1679e6db8a475c089641
kenneth <kenneth.adjete@ism.edu.sn>
2023-10-24 11:51:51 +0000

    Correction

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -118,7 +118,9 @@ const NavBar= () =>{
                                 <button onClick={() => setMode(mode === "light" ? "dark" : "light")} 
                                 className={`ml-2 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
                                 {
-                                    
+                                    mode === "dark" ?
+                                    <SunIcon  className={"fill-dark"}/>
+                                    : <MoonIcon className={"fill-dark"} />
                                 }  
 
                                 </button>
