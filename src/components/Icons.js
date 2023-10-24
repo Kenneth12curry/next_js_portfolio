e2ded868d856ccbcfe3dcc38bb9622f176f5d5b3
kenneth <kenneth.adjete@ism.edu.sn>
2023-10-24 17:03:35 +0000

    CorrectionIcs

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -259,7 +259,8 @@ export const CircularText = ({ className, ...rest }) => (
   </svg>
 );
   
-export const SunIcon = ({ className, ...rest }) => (
+
+/* export const SunIcon = ({ className, ...rest }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -435,9 +436,9 @@ export const SunIcon = ({ className, ...rest }) => (
       />
     </circle>
   </svg>
-);
+);*/
 
-export const MoonIcon = ({ className, ...rest }) => (
+/* export const MoonIcon = ({ className, ...rest }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -591,6 +592,68 @@ export const MoonIcon = ({ className, ...rest }) => (
       />
     </circle>
   </svg>
+); */
+
+export const SunIcon = ({ className, ...rest }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    {...rest}
+    className={`w-full h-auto ${className}`}
+  >
+    <circle cx="12" cy="12" r="6" fill="currentColor" />
+    <circle
+      cx="12"
+      cy="12"
+      r="12"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    />
+    <path
+      fill="currentColor"
+      d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
+      opacity="0"
+    />
+  </svg>
 );
 
-  
+export const MoonIcon = ({ className, ...rest }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    {...rest}
+    className={`w-full h-auto ${className}`}
+  >
+    <circle cx="12" cy="12" r="10" fill="currentColor" />
+    <circle
+      cx="12"
+      cy="12"
+      r="12"
+      fill="none"
+      stroke="currentColor"
+      strokeDasharray="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <animateTransform
+        attributeName="transform"
+        dur="30s"
+        repeatCount="indefinite"
+        type="rotate"
+        values="0 12 12;360 12 12"
+      />
+    </circle>
+    <g fill="currentColor">
+      <path d="M15.22 6.03L17.75 4.09L14.56 4L13.5 1L12.44 4L9.25 4.09L11.78 6.03L10.87 9.09L13.5 7.28L16.13 9.09L15.22 6.03Z">
+        <animate fill="freeze" attributeName="fill-opacity" dur="0.4s" values="1;0" />
+      </path>
+      <path d="M19.61 12.25L21.25 11L19.19 10.95L18.5 9L17.81 10.95L15.75 11L17.39 12.25L16.8 14.23L18.5 13.06L20.2 14.23L19.61 12.25Z">
+        <animate fill="freeze" attributeName="fill-opacity" begin="0.2s" dur="0.4s" values="1;0" />
+      </path>
+    </g>
+  </svg>
+);
