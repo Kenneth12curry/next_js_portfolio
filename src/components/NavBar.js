3f79315886ed3610044657b4c573ffdb8a21e1ec
kenneth <kenneth.adjete@ism.edu.sn>
2023-10-31 17:36:52 +0000

    Commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -112,8 +112,8 @@ const NavBar= () =>{
                             <nav className='flex items-center justify-center flex-wrap mt-2'>
                                 {/* Appel des 3 composants GithubIcon, LinkedInIcon, InstagramIcon */}
                                 <motion.a  href='https://github.com/Kenneth12curry'  className='w-6 mr-3 bg-light rounded-full dark:bg-dark sm:mx-1' whileHover={{y:-2}} whileTap={{scale:0.9}}><GithubIcon /></motion.a>
-                                <motion.a  href='www.linkedin.com/in/kenneth-adjete'  className='w-6 ml-3 sm:mx-1' whileHover={{y:-2}} whileTap={{scale:0.9}}><LinkedInIcon /></motion.a>
-                                <motion.a  href='https://twitter.com'  className='w-6 ml-3 sm:mx-1' whileHover={{y:-2}} whileTap={{scale:0.9}}><InstagramIcon /></motion.a>
+                                <motion.a  href='www.linkedin.com/in/kenneth-adjete'  className='w-6 sm:mx-1' whileHover={{y:-2}} whileTap={{scale:0.9}}><LinkedInIcon /></motion.a>
+                                <motion.a  href='https://twitter.com'  className='w-6 ml-2 sm:mx-1' whileHover={{y:-2}} whileTap={{scale:0.9}}><InstagramIcon /></motion.a>
 
                                 <button onClick={() => setMode(mode === "light" ? "dark" : "light")} 
                                 className={`ml-4 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
